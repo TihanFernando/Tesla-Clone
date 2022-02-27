8f0ec71b7e1d6b22478a0495d74b5ef50e4d6a51
TihanFernando <tihanlakmitha@gmail.com>
2022-02-27 20:14:42 +0530

    Making Menus

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,24 +1,29 @@
 import React from 'react'
 import styled from 'styled-components'
+import Fade from 'react-reveal/Fade';
 
 function Section({ title, description, leftBtnText, rightBtnText, backgroundImg}) {
   return (
     <Wrap bgImage={backgroundImg}>
+      <Fade bottom>
       <ItemText>
         <h1>{title}</h1>
         <p>{description}</p>
       </ItemText>
+      </Fade>
       <Buttons>
-        <ButtonGroup>
-          <LeftButton>
-            {leftBtnText}
-          </LeftButton>
-          {rightBtnText && 
-            <RightButton>
-            {rightBtnText}
-          </RightButton>
-          }
-        </ButtonGroup>
+        <Fade bottom>
+          <ButtonGroup>
+            <LeftButton>
+              {leftBtnText}
+            </LeftButton>
+            {rightBtnText && 
+              <RightButton>
+              {rightBtnText}
+            </RightButton>
+            }
+          </ButtonGroup>
+        </Fade>
         <DownArrow src="/images/down-arrow.svg" />
       </Buttons>
     </Wrap>
@@ -28,21 +33,22 @@ function Section({ title, description, leftBtnText, rightBtnText, backgroundImg}
 export default Section
 
 const Wrap = styled.div`
-    width: 100vw;
-    height: 100vh;
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-image: url('/images/model-s.jpg');
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between; //vertical
-    align-items: center; //horizontal
-    background-image: ${props => `url("/images/${props.bgImage}")`}
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-image: url('/images/model-s.jpg');
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between; //vertical
+  align-items: center; //horizontal
+  background-image: ${props => `url("/images/${props.bgImage}")`}
 `
 const ItemText = styled.div`
   padding-top: 15vh;
   text-aling: center;
+  z-index: -1;
 `
 const ButtonGroup = styled.div`
   display: flex;
