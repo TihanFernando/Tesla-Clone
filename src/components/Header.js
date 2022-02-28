4cf568437f06e2b4142757bd8ea4c47f27b381ce
TihanFernando <tihanlakmitha@gmail.com>
2022-02-28 20:37:24 +0530

    Final Commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
-import MenuIcon from '@material-ui/icons/Menu'
+import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
+import { Translate } from '@material-ui/icons';
+import { selectCars } from '../features/car/carSlice';
+import { useSelector } from 'react-redux';
 
 function Header() {
+  const [burgerStatus, setBurgurStatus] = useState(false);
+  const cars = useSelector(selectCars)
+  console.log(cars);
+
   return (
     <Container>
       <a>
         <img src="/images/logo.svg" alt="" />
       </a>
       <Menu>
-        <a href="#">Model S</a>
-        <a href="#">Model 3</a>
-        <a href="#">Model X</a>
-        <a href="#">Model Y</a>
+        {cars && cars.map((car, index)=>(
+          <a key={index} href="#">{car}</a>
+        ))}
       </Menu>
       <RightMenu>
         <a href="#">Shop</a>
         <a href="#">Tesla Account</a>
-        <CustomMenu>
-
-        </CustomMenu>
+        <CustomMenu onClick={()=>setBurgurStatus(true)} />
       </RightMenu>
-      <BurgurNav>
+      <BurgurNav show={burgerStatus}>
+        <CloseWrapper>
+          <CustomClose onClick={()=>setBurgurStatus(false)}/>
+        </CloseWrapper>
+        {cars && cars.map((car, index)=>(
+          <li key={index}><a href="#">{car}</a></li>
+        ))}
+
         <li><a href="#">Existing Inventory</a></li>
         <li><a href="#">Used Inventory</a></li>
         <li><a href="#">Trade-in</a></li>
         <li><a href="#">Cybertruck</a></li>
         <li><a href="#">Roadaster</a></li>
-        <li><a href="#">Existing Inventory</a></li>
-        <li><a href="#">Existing Inventory</a></li>
-        <li><a href="#">Existing Inventory</a></li>
       </BurgurNav>
     </Container>
   )
@@ -91,6 +100,8 @@ const BurgurNav = styled.div`
   display: flex;
   flex-direction: column;
   text-align: start;
+  transform: ${props => props.show ? 'translateX(0)':'translateX(100%)'};
+  transition: transform 0.2s;
   li{
     padding: 15px 0;
     border-bottom: 1px solid rgba(0, 0, 0, .2);
@@ -99,4 +110,12 @@ const BurgurNav = styled.div`
       font-weight: 600;
     }
   }
+`
+const CustomClose = styled(CloseIcon)`
+  cursor: pointer;
+`
+const CloseWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+
 `
